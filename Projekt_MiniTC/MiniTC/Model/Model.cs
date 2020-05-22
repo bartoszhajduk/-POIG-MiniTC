1fe3922fec0422593e96c06d835a6fbc4ce8a3e6
bartoszhajduk <61735413+bartoszhajduk@users.noreply.github.com>
2020-05-22 18:44:54 +0200

    Add files via upload

---
+++ Projekt_MiniTC/MiniTC/Model/Model.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace MiniTC.Model
+{
+    public class Model
+    {
+        Boolean IsFolder = true;
+        public string Name { get; private set; }
+        public List<string> Drives { get; private set; } = new List<string>();
+        public List<string> Files { get; private set; } = new List<string>();
+
+
+        private void prevPath()
+        {
+            string[] values = Name.Split('\\');
+            int len = values[values.Length - 2].Length;
+            Name = Name.Remove(Name.Length - len - 1, len + 1);
+        }
+ 
+        public void setPath(string path)
+        {
+            string tmp = Name;
+            try
+            {
+                if (IsFolder == false)
+                {
+                    string[] values = Name.Split('\\');
+                    int len = values[values.Length - 2].Length;
+                    Name = Name.Remove(Name.Length - len - 1, len + 1);
+                }
+                if (path.Contains("<D>"))
+                {
+                    IsFolder = true;
+                    Name += path.Remove(0, 3) + '\\';
+                }
+                else if (path == "...")
+                {
+                    IsFolder = true;
+                    string[] values = Name.Split('\\');
+                    int len = values[values.Length - 2].Length;
+                    Name = Name.Remove(Name.Length - len - 1, len + 1);
+                }
+                else if (path.Length <= 4)
+                {
+                    IsFolder = true;
+                    Name = path;
+                }
+                else
+                {
+                    IsFolder = false;
+                    Name += path + '\\';
+                }
+                Directory.GetAccessControl(Name);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Brak dostępu");
+                Name = tmp;
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Nie Znaleziono pliku");
+                IsFolder = true;
+                prevPath();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                IsFolder = true;
+                while (Directory.Exists(Name) == false)
+                {
+                    MessageBox.Show("Nie Znaleziono ścieżki");
+                    prevPath();
+                }
+                getCurrentFilesAndFolders();
+            }
+        }
+
+        public List<string> getLogicalDrives()
+        {
+            Drives.Clear();
+            string[] tmp = Directory.GetLogicalDrives();
+            foreach (var i in tmp)
+            {
+                Drives.Add(i);
+            }
+            return Drives;
+        }
+
+        public List<string> getCurrentFilesAndFolders()
+        {
+            try
+            {
+                if (IsFolder == true)
+                {
+                    Files.Clear();
+                    if (Name.Length > 4)
+                    {
+                        Files.Add("...");
+                    }
+                    string[] tmp = Directory.GetDirectories(Name);
+                    string[] tmp2 = Directory.GetFiles(Name);
+                    foreach (var i in tmp)
+                    {
+                        string name = new FileInfo(i).Name;
+                        Files.Add("<D>" + name);
+
+                    }
+                    foreach (var i in tmp2)
+                    {
+                        string name = new FileInfo(i).Name;
+                        Files.Add(name);
+                    }
+                }
+            }
+            catch(DirectoryNotFoundException)
+            {
+                MessageBox.Show("Folder nadrzędny już nie istnieje");
+                IsFolder = true;
+                while (Directory.Exists(Name) == false)
+                {
+                    prevPath();       
+                }
+                getCurrentFilesAndFolders();
+            }    
+            return Files;
+        }
+
+        public void CopyFile(Model Output)
+        {
+            try
+            {
+                if (IsFolder == false)
+                {
+                    string[] values = Name.Split('\\');
+                    if (Output.IsFolder == true)
+                    {
+                        File.Copy(Name, Output.Name + values[values.Length - 2]);
+                    }
+                    else
+                    {
+                        Output.prevPath();
+                        File.Copy(Name, Output.Name + values[values.Length - 2]);   
+                        
+                        Output.IsFolder = true;
+                        Output.Files.Clear();
+                        if (Output.Name.Length > 4)
+                        {
+                            Output.Files.Add("...");
+                        }
+                        string[] tmp = Directory.GetDirectories(Output.Name);
+                        string[] tmp2 = Directory.GetFiles(Output.Name);
+                        foreach (var i in tmp)
+                        {
+                            string name = new FileInfo(i).Name;
+                            Output.Files.Add("<D>" + name);
+
+                        }
+                        foreach (var i in tmp2)
+                        {
+                            string name = new FileInfo(i).Name;
+                            Output.Files.Add(name);
+                        }
+                    } 
+                }
+                else 
+                {
+                    MessageBox.Show("Nie wybrano pliku do kopiowania");
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Plik już istnieje w podanej ścieżce lub kopiowany element już nie istnieje");
+                IsFolder = true;
+                while (Directory.Exists(Name) == false)
+                {
+                    prevPath();
+                }
+                Output.IsFolder = true;
+                while (Directory.Exists(Output.Name) == false)
+                {
+                    Output.prevPath();
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Brak uprawnień dostępu");
+            }
+        }
+    }
+}
---
+++ Projekt_MiniTC/MiniTC/ModelView/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTC.ModelView
+{
+    using Model;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+
+    internal class Program : ViewModel
+    {
+        private Model modelIn = new Model();
+        private Model modelOut = new Model();
+
+        public List<string> Drives { get; set; } = new List<string>();
+        public string InputDrive { get; set; }
+        public string InputName { get; set; }
+        public ObservableCollection<string> InputFiles { get; set; }
+        public string OutputDrive { get; set; }
+        public string OutputName { get; set; }
+        public ObservableCollection<string> OutputFiles { get; set; }
+
+
+        private ICommand _comboBoxClick = null;
+        public ICommand ComboBoxClick
+        {
+            get
+            {
+                if (_comboBoxClick == null)
+                {
+                    _comboBoxClick = new RelayCommand(
+                        arg => { Drives=modelIn.getLogicalDrives(); modelOut.getLogicalDrives(); onPropertyChanged(nameof(Drives)); },
+                        arg => true
+                     );
+                }
+                return _comboBoxClick;
+            }
+        }
+       
+        private ICommand _changeInputDrive = null;
+        public ICommand ChangeInputDrive
+        {
+            get
+            {
+                if (_changeInputDrive == null)
+                {
+                    _changeInputDrive = new RelayCommand(
+                        arg => { modelIn.setPath(InputDrive); InputFiles = new ObservableCollection<string>(modelIn.getCurrentFilesAndFolders()) ; onPropertyChanged(nameof(InputFiles));},
+                        arg => true
+                     );
+                }
+                return _changeInputDrive;
+            }
+        }
+
+        private ICommand _changeOutputDrive = null;
+        public ICommand ChangeOutputDrive
+        {
+            get
+            {
+                if (_changeOutputDrive == null)
+                {
+                    _changeOutputDrive = new RelayCommand(
+                        arg => { modelOut.setPath(OutputDrive); OutputFiles = new ObservableCollection<string>(modelOut.getCurrentFilesAndFolders()); onPropertyChanged(nameof(OutputFiles)); },
+                        arg => true
+                     );
+                }
+                return _changeOutputDrive;
+            }
+        }
+        
+        private ICommand _changeInputPath = null;
+        public ICommand ChangeInputPath
+        {
+            get
+            {
+                if (_changeInputPath == null)
+                {
+                    _changeInputPath = new RelayCommand(
+                        arg => { modelIn.setPath(InputName); InputFiles = new ObservableCollection<string>(modelIn.getCurrentFilesAndFolders()); InputName = modelIn.Name; onPropertyChanged(nameof(InputFiles), nameof(InputName)); },
+                        arg => true
+                     );
+                }
+                return _changeInputPath;
+            }
+        }
+
+        private ICommand _changeOutputPath = null;
+        public ICommand ChangeOutputPath
+        {
+            get
+            {
+                if (_changeOutputPath == null)
+                {
+                    _changeOutputPath = new RelayCommand(
+                        arg => { modelOut.setPath(OutputName); OutputFiles = new ObservableCollection<string>(modelOut.getCurrentFilesAndFolders()); OutputName = modelOut.Name; onPropertyChanged(nameof(OutputFiles),nameof(OutputName)); },
+                        arg => true
+                     );
+                }
+                return _changeOutputPath;
+            }
+        }
+
+        private ICommand _copy = null;
+        public ICommand Copy
+        {
+            get
+            {
+                if (_copy == null)
+                {
+                    _copy = new RelayCommand(
+                        arg => { modelIn.CopyFile(modelOut); InputName = modelIn.Name; OutputName = modelOut.Name; InputFiles = new ObservableCollection<string>(modelIn.getCurrentFilesAndFolders()); OutputFiles = new ObservableCollection<string>(modelOut.getCurrentFilesAndFolders()); onPropertyChanged(nameof(InputName), nameof(OutputName), nameof(OutputFiles),nameof(InputFiles)); },
+                        arg => (!string.IsNullOrEmpty(InputName)) && (!string.IsNullOrEmpty(OutputName))
+                     );
+                }
+                return _copy;
+            }
+        }
+
+    }
+}
